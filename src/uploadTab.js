77d0cbb1f30aaf1cda8b08219be368b60fd68625
Marik-D <maretskii@gmail.com>
2019-11-23 16:48:36 +0100

    Upload images to web api

--- src/uploadTab.js
+++ src/uploadTab.js
@@ -1,20 +1,45 @@
 const { fromEvent } = require('rxjs')
 
+const API_URL = 'http://localhost:3000'
+
+async function uploadImage(image) {
+  const formData = new FormData()
+  formData.append('image', image)
+
+  const res = await fetch(`${API_URL}/image`, {
+    method: 'POST',
+    body: formData,
+  })
+  const json = await res.json()
+  return json.id;
+}
+
+async function createPost(title, imgId) {
+  await fetch(`${API_URL}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      src: `${API_URL}/image/${imgId}`,
+      title,
+    }),
+  })
+}
+
 let formSub;
 
 function showUploadTab() {
   document.getElementById('upload-tab').classList.remove('hidden')
 
   formSub = fromEvent(document.getElementById('upload-form'), 'submit').subscribe(async e => {
-    console.log(e)
     e.preventDefault();
-    await fetch('http://localhost:3000', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
-    })
+    const data = Object.fromEntries(new FormData(e.target));
+    console.log(data)
+    
+    const imgId = await uploadImage(data.image)
+
+    await createPost(data.title, imgId)
   })
 }
 
